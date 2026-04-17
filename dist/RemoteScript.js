77fb1a00f52b8761ac777e8d78c08d470d4249f3
Rowan Fisher <rowfisher29@gmail.com>
2026-04-17 14:08:08 -0400

    Test html overlay...was just showing text for my overlay 1

--- dist/RemoteScript.js
+++ dist/RemoteScript.js
@@ -18296,6 +18296,7 @@ std_string_c_str (StdString * self)
           initRespawnUpdates();
           MountainBossHooks();
           new BossBattleOverlay("https://wolfonline.tiiny.site/");
+          Logger("LOAD THE DAMN COTRRECT SCIPRASG");
           Logger("    ------------");
           Logger("\n[+] Successfully Completed All Hooks");
         });

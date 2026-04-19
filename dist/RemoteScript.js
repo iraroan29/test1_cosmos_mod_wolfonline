d2e2a462ee75d24ecf57c9c7c05de8bf09ea29a3
Rowan Fisher <rowfisher29@gmail.com>
2026-04-18 21:14:11 -0400

    Hopefully log webview touch

--- dist/RemoteScript.js
+++ dist/RemoteScript.js
@@ -18096,8 +18096,9 @@ std_string_c_str (StdString * self)
         PhotonNetwork.method("Destroy").overload("UnityEngine.GameObject").invoke(bossGO);
         return;
       }
-      if (boss === null && bossGO !== null) {
-        Logger("Set boss " + bossGO);
+      Logger("BOSS GO!!! " + bossGO.toString() + " WHAT IS THIS?? " + this.toString());
+      if (boss === null) {
+        Logger("Set boss ");
         BossRegistry.setBoss(this, scene);
         return this.method("Update").invoke();
       }
